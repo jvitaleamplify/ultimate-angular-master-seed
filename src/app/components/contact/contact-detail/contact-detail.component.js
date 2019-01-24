0a8f666125d61c0e603e1f2eeb5cc6b5e92da2e8
Jake Vitale <jvitale@amplify.com>
2019-01-24 14:15:35 -0500

    New Contact Form

---
+++ src/app/components/contact/contact-detail/contact-detail.component.js
@@ -0,0 +1,13 @@
+var contactDetail = {
+
+    bindings: {
+        contact: '<',
+        onSave: '&'
+    },
+    templateUrl: './contact-detail.html',
+    controller: 'ContactDetailController'
+};
+
+angular
+.module('components.contact')
+.component('contactDetail', contactDetail);
---
+++ src/app/components/contact/contact-new/contact-new.controller.js
@@ -0,0 +1,29 @@
+function ContactNewController(ContactService) {
+    var ctrl = this;
+    ctrl.$onInit = function () {
+        ctrl.contact = {
+            name: '',
+            email: '',
+            job: '',
+            location: '',
+            social: {
+                facebook: '',
+                github: '',
+                twitter: '',
+                linkedin: '',
+            },
+            tag: '',
+        };
+    };
+    
+    ctrl.createNewContact = function (event) {
+        return ContactService.createNewContact(event.contact).then(function (contact){
+            // success
+            console.log(contact);
+        });
+    };
+}
+
+angular
+.module('components.contact')
+.controller('ContactNewController', ContactNewController);
